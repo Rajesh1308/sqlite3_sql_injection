c8a2ad72a64f3be712042e51d68734eebf31fef4
Rajesh1308 <rajesharulrathna@gmail.com>
2022-12-30 22:35:11 +0530

    Intro added

--- sql_injection.py
+++ sql_injection.py
@@ -1,3 +1,14 @@
+
+
 import sqlite3
-print("Hello world")
-print("Testing commit")
+
+
+print("================================================================")
+print("=         Welcome to Demo on SQL injection with sqlite3        =")
+print("================================================================")
+
+print("\n\nThe projects aims to provide a hands on experience on sql injection")
+print("Here in the password section you can execute sql injection to gain access the data inside the project")
+print("Inside the project, in the search option you can perform union attack and view all the user ID and passwords")
+print("All the best and have a good testing\n\n")
+
