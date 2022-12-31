fc3f82f2e5b6757cbb143495883f012c3fd156c1
Rajesh1308 <rajesharulrathna@gmail.com>
2022-12-31 14:02:31 +0530

    Removed getpass and mainpage file

--- sql_injection.py
+++ sql_injection.py
@@ -1,8 +1,6 @@
 
 import os
 import sqlite3
-import getpass
-import main_page as mp
 
 
 def welcome():
@@ -32,18 +30,34 @@ def pass_check(uname,passwd):
         print(item[0])
         if item[0] == uname:
             clr_scr()
-            mp.master()
+            master()
 
     conn.commit()
     conn.close()
+
+def master():
+    conn = sqlite3.connect("Passdatabase.db")
+    c = conn.cursor()
+
+    print("This session contains the details of OWASP TOP 10 vulnerabilities of 2022")
+    print("Data source  : https://www.reflectiz.com/blog/owasp-top-ten-2022/")
+    sql_command = """SELECT rowid,* FROM data
+    """
+    data = c.execute(sql_command)
+    for item in data:
+        print(item[0], item[1])
+        print("     " + item[2])
+    conn.commit()
+    conn.close()
+    
 
 if __name__ == '__main__':
     welcome()
     print("Press any enter to continue...")
     input()
     clr_scr()
-    unam = input("Enter user name : ")
-    passwd = getpass.getpass()
+    unam = input("Enter user name  : ")
+    passwd = input("Enter password  : ")
     pass_check(unam,passwd)
 
 
