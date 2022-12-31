ff80ad594c8bf1e5bf88e8b16d4b87e681353e2b
Rajesh1308 <rajesharulrathna@gmail.com>
2022-12-31 13:57:07 +0530

    page source added

--- main_page.py
+++ main_page.py
@@ -3,6 +3,9 @@ import sqlite3
 def master():
     conn = sqlite3.connect("Passdatabase.db")
     c = conn.cursor()
+
+    print("This session contains the details of OWASP TOP 10 vulnerabilities of 2022")
+    print("Data source  : https://www.reflectiz.com/blog/owasp-top-ten-2022/")
     sql_command = """SELECT rowid,* FROM data
     """
     data = c.execute(sql_command)
